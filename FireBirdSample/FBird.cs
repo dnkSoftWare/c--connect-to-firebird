e85cc4f8da469852a0b7d468f0665dfc85b5526b
dnk <karuna@mail.ru>
2019-08-28 22:26:30 +0300

    fix FBird lib 1

---
+++ FireBirdSample/FBird.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+using FirebirdSql.Data.FirebirdClient;
+
+namespace FireBirdSample
+{
+    public class FBird
+    {
+        private StringBuilder connString;
+        private FbTransaction write_transaction;
+        private FbTransaction read_transaction;
+        public FbConnection conn { get; set; }
+        public FBird(Dictionary<string, string> connectionSettings)
+        {
+            connString = new StringBuilder("");
+            foreach (KeyValuePair<string, string> pair in connectionSettings)
+            {
+                FbConnectionStringBuilder.AppendKeyValuePair(connString, pair.Key, pair.Value);
+            }
+            conn = new FbConnection(connString.ToString());
+            
+        }
+
+        private FbTransaction GetTransaction(bool readable = true)
+        {
+            if (readable)
+            {
+                var tro = new FbTransactionOptions()
+                {
+                    TransactionBehavior = FbTransactionBehavior.Read | FbTransactionBehavior.ReadCommitted | FbTransactionBehavior.RecVersion
+                };
+
+                read_transaction = conn.BeginTransaction(tro);
+                return read_transaction;
+            }
+            else
+            {
+                var tro = new FbTransactionOptions()
+                {
+                    TransactionBehavior = FbTransactionBehavior.NoWait | FbTransactionBehavior.ReadCommitted | FbTransactionBehavior.RecVersion
+                };
+
+                write_transaction = conn.BeginTransaction(tro);
+                return write_transaction;
+
+            }
+        }
+
+        public FbDataReader Select(string sql)
+        {
+            
+
+            var cmd = new FbCommand(sql, conn, GetTransaction());
+            
+            return cmd.ExecuteReader(); // DataReader
+        }
+    }
+}
--- FireBirdSample/Program.cs
+++ FireBirdSample/Program.cs
@@ -12,29 +12,52 @@ namespace FireBirdSample
     {
         static void Main(string[] args)
         {
-            string connectionString =
-                "User=SYSDBA;" +
-                "Password=masterkey;" +
-                "Database=F:\\Delphi\\Projects\\dnk-Учет-nb\\db\\UCHET_MS.GDB;" +
-                "DataSource=localhost;" +
-                "Port=3050;" +
-                "Dialect=3;" +
-                "Charset=WIN1251;" +
-                "Role=;" +
-                "Connection lifetime=15;" +
-                "Pooling=true;" +
-                "MinPoolSize=0;" +
-                "MaxPoolSize=50;" +
-                "Packet Size=8192;" +
-                "ServerType=0";
+            Dictionary<string, string> connectionSettings = new Dictionary<string, string>()
+            {
+                 {"User","SYSDBA"},
+                 {"Password","masterkey"},
+                 {"Database","F:\\Delphi\\Projects\\dnk-Учет-nb\\db\\UCHET_MS.GDB"},
+                 {"DataSource","localhost"},
+                 {"Port","3050"},
+                 {"Dialect","3"},
+                 {"Charset","WIN1251"},
+                 {"Role",""},
+                 {"Connection lifetime","15"},
+                 {"Pooling","true"},
+                 {"MinPoolSize","0"},
+                 {"MaxPoolSize","50"},
+                 {"Packet Size","8192"},
+                 {"ServerType","0"}
+            };
+            FBird fb = new FBird(connectionSettings);
 
-            var myConnection1 = new FbConnection(connectionString);
-            myConnection1.Open();
+            //            Console.ReadKey(); 
+            //            throw new ApplicationException("Exit"); 
+            //
+            //            string connectionString =
+            //                "User=SYSDBA;" +
+            //                "Password=masterkey;" +
+            //                "Database=F:\\Delphi\\Projects\\dnk-Учет-nb\\db\\UCHET_MS.GDB;" +
+            //                "DataSource=localhost;" +
+            //                "Port=3050;" +
+            //                "Dialect=3;" +
+            //                "Charset=WIN1251;" +
+            //                "Role=;" +
+            //                "Connection lifetime=15;" +
+            //                "Pooling=true;" +
+            //                "MinPoolSize=0;" +
+            //                "MaxPoolSize=50;" +
+            //                "Packet Size=8192;" +
+            //                "ServerType=0";
+            //
+            //            var myConnection1 = new FbConnection(connectionString);
+            //            myConnection1.Open();
+
+            fb.conn.Open();
             try
             {
-                
-                var cmd = new FbCommand("select * from USERS;", myConnection1);
-                FbDataReader dr = cmd.ExecuteReader(); // DataReader
+
+               IDataReader dr = fb.Select("select * from USERS;");
 
                 while (dr.Read())
                 {
@@ -43,8 +66,8 @@ namespace FireBirdSample
                 dr.Close();
             }
             finally
-            {                
-               myConnection1.Close();
+            {
+                fb.conn.Close();
                Console.ReadLine();
             }
             // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
