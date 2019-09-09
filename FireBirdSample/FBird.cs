0cd626057b399856c7bb7ced565aab62ec6c080c
dnk <karuna@mail.ru>
2019-09-09 21:26:23 +0300

    fix FBird  lib 09-09-2019 21:25

--- FireBirdSample/FBird.cs
+++ FireBirdSample/FBird.cs
@@ -6,7 +6,7 @@ using FirebirdSql.Data.FirebirdClient;
 
 namespace FireBirdSample
 {
-    public class FBird
+    public class FBird : IDisposable
     {
         private StringBuilder connString;
         private FbTransaction write_transaction;
@@ -27,34 +27,69 @@ namespace FireBirdSample
         {
             if (readable)
             {
+                if (write_transaction != null)
+                {
+                    // if(conn.)
+                    write_transaction.RollbackRetaining();
+                    write_transaction.Dispose();
+                }
+
                 var tro = new FbTransactionOptions()
                 {
                     TransactionBehavior = FbTransactionBehavior.Read | FbTransactionBehavior.ReadCommitted | FbTransactionBehavior.RecVersion
                 };
 
                 read_transaction = conn.BeginTransaction(tro);
-                return read_transaction;
+                
             }
             else
             {
+                if (read_transaction != null)
+                {
+                    read_transaction.CommitRetaining();
+                    read_transaction.Dispose();
+                }
+
                 var tro = new FbTransactionOptions()
                 {
                     TransactionBehavior = FbTransactionBehavior.NoWait | FbTransactionBehavior.ReadCommitted | FbTransactionBehavior.RecVersion
                 };
-
+                
                 write_transaction = conn.BeginTransaction(tro);
-                return write_transaction;
+                
 
             }
+            return write_transaction ?? read_transaction;
         }
 
         public FbDataReader Select(string sql)
         {
             
-
             var cmd = new FbCommand(sql, conn, GetTransaction());
             
             return cmd.ExecuteReader(); // DataReader
+        }
+
+        public int Execute(string sql, ref FbTransaction tr, bool commit_auto = true)
+        {
+            if (tr == null)
+             tr = GetTransaction(false);
+
+            var cmd = new FbCommand(sql, conn, tr);
+            var rowAffected = cmd.ExecuteNonQuery();
+
+            if (commit_auto)
+            {
+                tr.CommitRetaining();
+            }
+
+            return rowAffected;
+
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
         }
     }
 }
--- FireBirdSample/Program.cs
+++ FireBirdSample/Program.cs
@@ -29,48 +29,53 @@ namespace FireBirdSample
                  {"Packet Size","8192"},
                  {"ServerType","0"}
             };
-            FBird fb = new FBird(connectionSettings);
 
-            //            Console.ReadKey(); 
-            //            throw new ApplicationException("Exit"); 
-            //
-            //            string connectionString =
-            //                "User=SYSDBA;" +
-            //                "Password=masterkey;" +
-            //                "Database=F:\\Delphi\\Projects\\dnk-Учет-nb\\db\\UCHET_MS.GDB;" +
-            //                "DataSource=localhost;" +
-            //                "Port=3050;" +
-            //                "Dialect=3;" +
-            //                "Charset=WIN1251;" +
-            //                "Role=;" +
-            //                "Connection lifetime=15;" +
-            //                "Pooling=true;" +
-            //                "MinPoolSize=0;" +
-            //                "MaxPoolSize=50;" +
-            //                "Packet Size=8192;" +
-            //                "ServerType=0";
-            //
-            //            var myConnection1 = new FbConnection(connectionString);
-            //            myConnection1.Open();
-
-            fb.conn.Open();
-            try
-            {
-
-               IDataReader dr = fb.Select("select * from USERS;");
-
-                while (dr.Read())
-                {
-                  Console.WriteLine("ID:"+ dr.GetValue(0).ToString()+" Name:"+ dr.GetString(1));  
-                }
-                dr.Close();
-            }
-            finally
-            {
-                fb.conn.Close();
-               Console.ReadLine();
-            }
+            Sample1(connectionSettings);
             // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+        }
+
+        private static void Sample2(Dictionary<string, string> connectionSettings)
+        {
+
+        }
+
+        private static void Sample1(Dictionary<string, string> connectionSettings)
+        {
+            using (FBird fb = new FBird(connectionSettings))
+            {
+                fb.conn.Open();
+                try
+                {
+                    #region DataReder Sample
+                    IDataReader dr = fb.Select("select * from USERS;");
+
+                    while (dr.Read())
+                    {
+                        Console.WriteLine("ID:" + dr.GetValue(0).ToString() + " Name:" + dr.GetString(1));
+                    }
+
+                    dr.Close(); 
+                    #endregion
+                    Console.ReadLine();
+                    #region Добавление 2-х записей в одной транзакции
+                    FbTransaction tr = null;
+                    var col = fb.Execute(@"
+                INSERT INTO USERS(NAME, PSW, LEVEL, ONLINE, CURRVERSION, ROLE_NAME, USER_NAME, PSW_USER, IP) 
+              VALUES('Каруна Т.А.', '222345', 1, 0, NULL, 'R', 'READER', '123', '192.168.2.40'); ", ref tr, false);
+
+                    col += fb.Execute(@"
+                INSERT INTO USERS(NAME, PSW, LEVEL, ONLINE, CURRVERSION, ROLE_NAME, USER_NAME, PSW_USER, IP) 
+              VALUES('Каруна Т.П.', '123345', 1, 0, NULL, 'R', 'READER', '123', '192.168.2.40'); ", ref tr, true);
+
+                    Console.WriteLine("Добалено {0} записей!", col); 
+                    #endregion
+                }
+                finally
+                {
+                    fb.conn.Close();
+                    Console.ReadLine();
+                }
+            }
         }
     }
 }
